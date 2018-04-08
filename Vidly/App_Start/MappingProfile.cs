8df0e9a6286a8185254e9fd4f80e182235d77356
Manav Panchal <mpancha1@binghamton.edu>
2018-04-07 22:44:01 -0400

    Added Data Transfer Object to Transfer the Data. used Auto Mapper to Map them

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -8,12 +8,17 @@ using Vidly.Models;
 
 namespace Vidly.App_Start
 {
-    public class MappingProfile:Profile
+    public class MappingProfile : Profile
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
             Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore()); 
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore()); ;
+
+
+
         }
     }
 }
